b5f8bacef8baf50659b7a1788db0540d849d69cc
dshu <hudongsheng@xfruit.cn>
2018-05-02 09:19:58 +0800

    init

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
-const Koa = require('koa');
-const router = require('koa-router')();
-const bodyParser = require('koa-bodyparser');
-const app = new Koa();
-const controller = require('./controller');
+/**
+ * koa的核心是中间件的堆砌,理解每个中间件，一个一个实现即可~
+ */
+const Koa = require('koa')
+const path = require('path')
+const app = new Koa()
+const route = require('koa-route')
+const serve = require('koa-static')
+const compose = require('koa-compose')
+const koaBody = require('koa-body')
+const os = require('os')
+app.use(koaBody({ multipart: true}))
 
-// log request URL:
-app.use(async (ctx, next) => {
-    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
-    await next();
-});
+console.log('default temporary file path---->',os.tmpdir())
+console.log('path.__dirname------->',__dirname)
+const publicServer = serve(path.join(__dirname))
 
-// parse request body:
-app.use(bodyParser());
+const logger = (ctx,next) =>{
+    console.log(`start time:${Date.now()} \n ${ctx.request.method}  ${ctx.request.url}`)
+    next()
+}
 
-app.use(controller());
-app.listen(3000);
-console.log('app started at port 3000...');
+const main = async ctx=>{
+    const tmpdir = os.tmpdir()
+    const filePaths = []
+    const files = ctx.request.body.files || {}
 
+    for(let key in files){
+        let file = files[key]
+        let filePath = path.join(tmpdir,file.name)
+        let reader = fs.createReadStream(file.path)
+        let writer = fs.createWriteStream(filePath)
+        reader.pipe(writer)
+        filePaths.push(filePath)
+    }
+
+    ctx.body = filePaths
+}
+const middlewares = compose([logger, publicServer])
+
+app.use(route.all('/', main))
+app.use(middlewares)
+
+
+app.listen(3000)
